e089ce6992e926352900bbd3610adf2e8d24152d
jpalanca <jpalanca@dsic.upv.es>
2023-10-23 17:51:51 +0200

    Added cli

--- clyps/cli.py
+++ clyps/cli.py
@@ -1,16 +1,56 @@
-"""Console script for clyps."""
-import sys
+from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
 import click
+from clyps.kb import KnowledgeBase
+from clyps.fact import Fact
+from clyps.rule import Rule
 
 
 @click.command()
-def main(args=None):
-    """Console script for clyps."""
-    click.echo("Replace this message by putting your code into "
-               "clyps.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+def cli():
+    """REPL interface for CLYPS with CLIPS-like syntax."""
+    kb = KnowledgeBase()
+    history = InMemoryHistory()
+    click.echo("CLYPS REPL with CLIPS-like syntax. Type (exit) or (quit) to leave.")
+    while True:
+        try:
+            # Prompt the user for input using prompt_toolkit
+            command = prompt("? ", history=history)
+
+            # Exit the REPL
+            if command in ["(exit)", "(quit)"]:
+                break
+
+            # Add a fact
+            elif command.startswith("(assert"):
+                fact_str = command[len("(assert ") : -1].strip()
+                fact = Fact.from_string(fact_str)
+                kb.add_fact(fact)
+                click.echo(f"=> Fact asserted: {fact}")
+
+            # Add a rule
+            elif command.startswith("(defrule"):
+                rule = Rule.from_string(command)
+                kb.add_rule(rule)
+                click.echo(f"=> Rule defined: {rule}")
+
+            # Perform inference
+            elif command == "(run)":
+                kb.infer()
+                click.echo("=> Inference completed.")
+                click.echo("Current facts:")
+                for fact in kb.facts:
+                    click.echo(fact)
+
+            # Unknown command
+            else:
+                click.echo(
+                    "Unknown command. Use (assert <fact>), (defrule <name> <rule>), (run), (exit) or (quit)."
+                )
+
+        except Exception as e:
+            click.echo(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()
